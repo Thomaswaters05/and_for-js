a78a6643676fee177afc2fdeccc18c5a977096e4
Tommy Waters <Thomas.Waters05@gmail.com>
2016-08-09 19:47:10 -0500

    adding initial repo and content

---
+++ for-loops.js
@@ -0,0 +1,12 @@
+console.log("*** Initialize at five and add Increments of 10 until you reach 120***");
+
+max = 120;
+
+for (var i = 5; i <= max; i++) {
+  max[i]
+}
+
+
+console.log("Result: " + max);
+
+
